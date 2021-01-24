e45010a35b883403c740d86bbcbcafb4a77c8f9d
Álvaro Fernández Rojas <noltari@gmail.com>
2021-01-24 12:12:22 +0100

    common: convert_size: remove byte output
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- nand_tools/common.py
+++ nand_tools/common.py
@@ -12,8 +12,8 @@ def auto_int(int_arg):
 def convert_size(size_bytes):
     """Convert size to human readable."""
     if size_bytes == 0:
-        return "0B"
-    size_name = ("B", "K", "M", "G", "T", "P", "E", "Z", "Y")
+        return "0"
+    size_name = ("", "K", "M", "G", "T", "P", "E", "Z", "Y")
     size_offset = int(math.floor(math.log(size_bytes, 1024)))
     size_pow = math.pow(1024, size_offset)
     size_value = round(size_bytes / size_pow, 2)
